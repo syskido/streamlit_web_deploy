497820d991e1e31173b63e1b853e2cd88fc483b1
kido <61651321+syskido@users.noreply.github.com>
2023-10-02 22:11:50 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np    
+
+from time import sleep  
+
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon="🧊",
+    page_title="나령 윤기도 스트림릿 배포하기",
+    layout="wide",
+)
